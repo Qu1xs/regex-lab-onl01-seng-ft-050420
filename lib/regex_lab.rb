645371d01a913db9f561e20b1586bce5087d7e99
kevin murphy <kevinnealmurphy@gmail.com>
2020-05-11 23:35:41 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -7,8 +7,8 @@ def words_starting_with_un_and_ending_with_ing(text)
 end
 
 def words_five_letters_long(text)
-  ary = text.split(" ")
-  ary.grep(/^\w{5}\s/)
+  #ary = text.split(" ")
+  text.split(" ").grep(/^\w{5}\s/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
