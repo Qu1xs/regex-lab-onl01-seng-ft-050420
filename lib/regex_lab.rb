644bb09d578cecc984c3b935cdfde93d463040f4
kevin murphy <kevinnealmurphy@gmail.com>
2020-05-11 23:33:50 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -15,5 +15,5 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-  phone.grep(/[0-9]{10}/) ? true : false
+  phone.grep(/[0-9]{10}&&[^a-z]/) ? true : false
 end
